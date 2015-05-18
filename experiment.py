5cec3152b51a2f33419e9dcbcd0b25d5f67dd2af
Kevin Bache <kevin.bache@gmail.com>
2015-05-18 15:13:43 -0700

    transfer

--- experiment.py
+++ experiment.py
@@ -244,9 +244,9 @@ class Experiment(object):
             error_log_file = os.path.join(final_output_path, "error.log")
             output_log_file = os.path.join(final_output_path, "output.log")
             sge_command = 'qsub -b n -V -p %d -N "%s" -wd "%s" -e "%s" -o "%s" "%s"' \
-                          % (int(priority), run_name,
+                          % (int(priority), run_name, final_output_path,
                              error_log_file, output_log_file,
-                             final_output_path, sge_scipt_file)
+                             sge_scipt_file)
 
             print_named_content('SGE Command:', sge_command)
 
