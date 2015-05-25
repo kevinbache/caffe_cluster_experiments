b4cabf77dcbdf907ea1397ccdd61de9b64f12409
Kevin Bache <kevin.bache@gmail.com>
2015-05-25 12:03:55 -0700

    transfer

--- cifar_alex/run_agl.py
+++ cifar_alex/run_agl.py
@@ -41,8 +41,8 @@ cross_params = {
     # 'batch_size': [25, 50, 125, 250, 500, 1000],
     'batch_size': [125],
     'log_low_alpha': [-6],
-    'log_high_alpha': [0],
-    'n_alphas': [99],
+    'log_high_alpha': [6],
+    'n_alphas': [199],
     # 'seed': np.arange(3)
     'seed': np.arange(1)
 }
