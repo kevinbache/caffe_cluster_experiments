bf3bd7e623342b2f67af668c0b95567bce5636fc
Kevin Bache <kevin.bache@gmail.com>
2015-05-28 00:35:28 -0700

    added adam to cifar

---
+++ cifar_c3f1/adam.py
@@ -0,0 +1,56 @@
+from __future__ import division
+import os
+import sys
+import numpy as np
+
+##################################
+# set up paths and extra imports #
+##################################
+def parent(dirname, n_levels):
+    for _ in range(n_levels):
+        dirname = os.path.dirname(dirname)
+    return dirname
+
+this_path = os.path.dirname(os.path.realpath(__file__))
+base_dir = parent(this_path, 1)
+sys.path.append(base_dir)
+sys.path.append(this_path)
+
+from experiment import Experiment, NamedTemplate, append_dicts, cross_dict
+from shared_params import *
+
+####################
+# set up templates #
+####################
+with open(os.path.join(this_path, 'solver_adam_template.prototxt'), 'r') as f:
+    algorithm_yaml_template_str = f.read()
+
+
+algorithm_name_template_str = "ADAM(" \
+                              "batch=${train_batch_size}_" \
+                              "lr=${base_lr}_"
+                              "lr_policy=${lr_policy}_"
+                              "beta1=${beta1}_" \
+                              "beta2=${beta2}_" \
+                              "nepochs=${n_epochs}" \
+                              ")"
+algorithm_template = NamedTemplate(algorithm_name_template_str, algorithm_yaml_template_str)
+
+##########
+# params #
+##########
+cross_params = {
+    'train_batch_size': [125, 250],
+    'momentum': [0.95],
+    'delta': [1e-8],
+    'seed': np.arange(3)
+}
+priority = 10
+hyper_params = append_dicts(hyper_params, cross_dict(cross_params))
+
+##################
+# run experiment #
+##################
+e = Experiment(use_sge=True, DEBUG_MODE=False)
+e.run(experiment_base_name, problem_template, algorithm_template, hyper_params,
+      offer_compatible_runs=False, priority=priority)
