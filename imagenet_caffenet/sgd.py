bbfa86027c0d222817bec9667cbf95c299e75573
Kevin Bache <kevin.bache@gmail.com>
2015-05-29 16:31:49 -0700

    transfer

--- imagenet_caffenet/sgd.py
+++ imagenet_caffenet/sgd.py
@@ -38,16 +38,31 @@ algorithm_template = NamedTemplate(algorithm_name_template_str, algorithm_yaml_t
 # params #
 ##########
 cross_params = {
-    'train_batch_size': [125, 250],
-    'learning_rate': np.logspace(-1, -3, 7),  # spacing of 2.15x
-    'lr_decay': [.1, .1**.5, 1],
+    'train_batch_size': [256],
+    # 'learning_rate': np.logspace(-1, -3, 7),  # spacing of 2.15x
+    'learning_rate': [0.01],  # spacing of 2.15x
+    'lr_decay': [.1],
     'lr_policy': ['step'],
-    'stepsize': [40000],
-    'momentum': [0.0],
+    'stepsize': [100000],
+    'momentum': [0.9],
     'seed': np.arange(1)
 }
 priority = 1
 hyper_params = append_dicts(hyper_params, cross_dict(cross_params))
+
+# test_iter: 1000
+# test_interval: 1000
+# base_lr: 0.01
+# lr_policy: "step"
+# gamma: 0.1
+# stepsize: 100000
+# display: 20
+# max_iter: 450000
+# momentum: 0.9
+# weight_decay: 0.0005
+# snapshot: 10000
+
+
 
 ##################
 # run experiment #
