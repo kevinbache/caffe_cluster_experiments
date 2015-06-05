c249977cf1017eb465a9ef9014a4f27a51ac779d
Kevin Bache <kevin.bache@gmail.com>
2015-06-04 18:49:29 -0700

    added ducb to runall.sh scripts

--- cifar_c3f1/runall.sh
+++ cifar_c3f1/runall.sh
@@ -2,6 +2,7 @@
 python agl.py
 python adl.py
 python lc.py
+python ducb.py
 python ad.py
 python ag.py
 python sgd.py
--- mnist_ff/runall.sh
+++ mnist_ff/runall.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 python agl.py
 python adl.py
-python ducb.py
 python lc.py
+python ducb.py
 python ad.py
 python ag.py
 python sgd.py
